0c9637bc5b1192970beaa61e4f9d4c3eacb4216f
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2021-11-26 18:59:06 -0300

    adding css colors variables

--- app/styles/global.ts
+++ app/styles/global.ts
@@ -1,9 +1,26 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
+    html {
+        --white: #FFFFFF;
+        --blue: #9FCBFF;
+        --blue2: #68ACFD;
+        --green: #8DD69C;
+        --green2: #31AA52;
+        --yellow: #FECD4C;
+        --red: #E75353;
+        --red2: #E02828;
+        --dark-blue: #434FAA;
+        --gray: #2D363C;
+        --black: #000000;
+        --bg: #F8F8F8;
+    }
     * {
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+    }
+    body {
+        background: var(--bg);
     }
 `;
