6b701b29a747b8de0a3b9739374e52a20c94d67f
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2021-11-26 18:47:41 -0300

    created global style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { GlobalStyle } from '../app/styles/global';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp
