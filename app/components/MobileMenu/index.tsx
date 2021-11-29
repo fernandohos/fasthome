f3e8b71c83cbe5f78feec1c2e6d35a1e7b6ddb77
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2021-11-29 19:31:26 -0300

    mobile profile tab

--- app/components/MobileMenu/index.tsx
+++ app/components/MobileMenu/index.tsx
@@ -4,68 +4,124 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { AnimatePresence } from 'framer-motion';
 import defaultProfileImage from '../../../public/images/default-user-image.svg';
+import arrowIcon from '../../../public/images/arrow.svg';
+import loginIcon from '../../../public/images/login-icon.svg';
+import signupIcon from '../../../public/images/signup-icon.svg';
+import infoIcon from '../../../public/images/info-icon.svg';
 
 export function MobileMenu() {
-    const userImage = null;
-    const [showMenu, setShowMenu] = useState(false);
+   const userImage = null;
+   const [showMenu, setShowMenu] = useState(false);
+   const [showUserDetails, setShowUserDetails] = useState<boolean>(false);
 
-    return (
-        <C.Container>
-            <C.Icon onClick={e => setShowMenu(v => !v)} show={showMenu}>
-                <div className="line"></div>
-                <div className="line"></div>
-                <div className="line"></div>
-            </C.Icon>
-            <AnimatePresence>
-                {
-                    showMenu && (
-                        <C.MobileMenu
-                            initial={{ opacity: 0, x: 10 }}
-                            animate={{ opacity: 1, x: 0 }}
-                            exit={{ opacity: 0, x: 10 }}
-                            transition={{
-                                x: { type: "spring", stiffness: 100 },
-                                default: { duration: .2 },
-                            }}
-                        >
-                            <div className="user-image">
-                                <Image src={userImage ?? defaultProfileImage} layout="fill" alt="user profile image" />
-                            </div>
-                            <ul>
-                                <li>
-                                    <Link href="/for-sale">
-                                        <a>
-                                            For Sale
-                                        </a>
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link href="/for-sale">
-                                        <a>
-                                            For Rent
-                                        </a>
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link href="/daily-rental">
-                                        <a>
-                                            Daily Rental
-                                        </a>
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link href="/projects">
-                                        <a>
-                                            Projects
-                                        </a>
-                                    </Link>
-                                </li>
-                            </ul>
-                        </C.MobileMenu>
-                    )
-                }
-            </AnimatePresence>
+   return (
+      <C.Container>
+         <C.Icon onClick={e => setShowMenu(v => !v)} show={showMenu}>
+            <div className="line"></div>
+            <div className="line"></div>
+            <div className="line"></div>
+         </C.Icon>
+         <AnimatePresence>
+            {
+               showMenu && (
+                  <C.MobileMenu
+                     initial={{ opacity: 0, x: 10 }}
+                     animate={{ opacity: 1, x: 0 }}
+                     exit={{ opacity: 0, x: 10 }}
+                     transition={{
+                        default: { duration: .2 },
+                     }}
+                  >
+                     <C.UserInfo opened={showUserDetails}>
+                        <div className="user-info-summary" onClick={e => setShowUserDetails(v => !v)}>
+                           <div className="user-image">
+                              <Image src={userImage ?? defaultProfileImage} layout="fill" alt="user profile image" />
+                           </div>
+                           <div className="arrow-icon">
+                              <Image src={arrowIcon} alt="arrow" layout="fill" />
+                           </div>
+                        </div>
 
-        </C.Container >
-    )
+                        <div className="a">
+                           <AnimatePresence>
+                              {
+                                 showUserDetails && (
+                                    <C.UserInfoDetail
+                                       initial={{ maxHeight: 0 }}
+                                       animate={{ maxHeight: 600 }}
+                                       exit={{ maxHeight: 0 }}
+                                       transition={{
+                                          duration: .3,
+                                          delay: 0
+                                       }}
+                                    >
+                                       <Link href="/login" passHref>
+                                          <C.UserDetailTab>
+                                             <div className="detail-tab-image">
+                                                <Image src={loginIcon} layout="fill" alt="login icon" />
+                                             </div>
+                                             Login
+                                          </C.UserDetailTab>
+                                       </Link>
+                                       <Link href="/signup" passHref>
+                                          <C.UserDetailTab>
+                                             <div className="detail-tab-image">
+                                                <Image src={signupIcon} layout="fill" alt="login icon" />
+                                             </div>
+                                             Signup
+                                          </C.UserDetailTab>
+                                       </Link>
+                                       <Link href="/profile-information" passHref>
+                                          <C.UserDetailTab>
+                                             <div className="detail-tab-image">
+                                                <Image src={infoIcon} layout="fill" alt="login icon" />
+                                             </div>
+                                             Profile information
+                                          </C.UserDetailTab>
+                                       </Link>
+
+                                    </C.UserInfoDetail>
+                                 )
+                              }
+                           </AnimatePresence>
+                        </div>
+                     </C.UserInfo>
+
+                     <ul>
+                        <li>
+                           <Link href="/for-sale">
+                              <a>
+                                 For Sale
+                              </a>
+                           </Link>
+                        </li>
+                        <li>
+                           <Link href="/for-sale">
+                              <a>
+                                 For Rent
+                              </a>
+                           </Link>
+                        </li>
+                        <li>
+                           <Link href="/daily-rental">
+                              <a>
+                                 Daily Rental
+                              </a>
+                           </Link>
+                        </li>
+                        <li>
+                           <Link href="/projects">
+                              <a>
+                                 Projects
+                              </a>
+                           </Link>
+                        </li>
+                     </ul>
+                  </C.MobileMenu>
+               )
+            }
+         </AnimatePresence>
+
+      </C.Container >
+   )
 }
--- app/components/MobileMenu/styles.ts
+++ app/components/MobileMenu/styles.ts
@@ -40,7 +40,7 @@ export const MobileMenu = styled(motion.div)`
     position: absolute;
     right: 0;
     top: 140%;
-    width: min(90vw, 20rem);
+    width: min(90vw, 25rem);
     padding: 2rem;
     background: var(--white);
     border-radius: .5rem;
@@ -60,16 +60,99 @@ export const MobileMenu = styled(motion.div)`
             
             a {
                 text-decoration: none;
+                color: var(--black);
             }
         }
     }
+`;
 
-    .user-image {
-            position: relative;
-            border-radius: 50%;
-            overflow: hidden;
-            height: 4rem;
-            width: 4rem;
-            background: #ddd;
+const close = keyframes`
+    from {
+        transform: rotate(0);
+    }
+    to {
+        transform: rotate(90deg);
+    }
+`;
+
+const open = keyframes`
+    from {
+        transform: rotate(90deg);
+    }
+    to {
+        transform: rotate(0);
+    }
+`;
+
+const userInfoClosed = keyframes`
+    from {
+        max-height: 100px;
+    }
+    to {
+        max-height: 0;
+    }
+`;
+
+const userInfoOpened = keyframes`
+    from {
+        max-height: 0;
+    }
+    to {
+        max-height: 100px;
+    }
+`;
+
+
+export const UserInfo = styled.div<{ opened: boolean | null }>`
+    margin-bottom: 1rem;
+
+    .user-info-summary {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+        .user-image {
+                position: relative;
+                border-radius: 50%;
+                overflow: hidden;
+                height: 3.8rem;
+                width: 3.8rem;
+                background: #ddd;
         }
+
+        .arrow-icon {
+            position: relative;
+            width: 1.6rem;
+            height: 5rem;
+            transform: rotate(90deg);
+            animation: ${({ opened }) => opened !== null ? opened ? open : close : null} .3s forwards;
+        }
+    }
+`;
+
+export const UserInfoDetail = styled(motion.div)`
+    overflow: hidden;
+`;
+
+export const UserDetailTab = styled.a`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    padding: .5rem 1rem;
+    border-radius: .5rem;
+    margin: .3rem 0;
+    transition: background-color ease .3s;
+    text-decoration: none;
+    color: var(--black);
+
+    .detail-tab-image {
+        position: relative;
+        width: 2rem;
+        height: 2rem;
+        margin-right: 1rem;
+    }
+
+    &:hover {
+        background: #eee;
+    }
 `;
