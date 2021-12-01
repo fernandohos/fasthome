2b59faa3aea5983b5b46c93f977703178c37e4fd
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2021-12-01 20:17:26 -0300

    import banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import type { NextPage } from 'next';
 import { Header } from '../app/patterns/Header';
+import { Banner } from '../app/patterns/Banner';
 
 const Home: NextPage = () => {
 
   return (
     <div>
       <Header />
+      <Banner />
     </div>
   )
 }
