a5d05f4b8321a0813b30ce571414f06d2751794a
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2021-12-02 19:19:44 -0300

    card house type

---
+++ app/types/CardHouseType.ts
@@ -0,0 +1,11 @@
+export type CardHouseType = {
+    id: string;
+    image: string;
+    name: string;
+    price: number;
+    location: string;
+    createdAt: number;
+    bedrooms: number;
+    floor: number;
+    squareMeters: number;
+}
