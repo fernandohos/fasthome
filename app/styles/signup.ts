8574557122ff39889e6bf6183929387d0b8230ad
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2021-12-07 19:01:37 -0300

    implementing AuthLayout

--- app/styles/signup.ts
+++ app/styles/signup.ts
@@ -1,113 +1,5 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 100vh;
-
-    .close-btn {
-        cursor: pointer;
-        position: absolute;
-        top: 2rem;
-        right: 2rem;
-        width: 3rem;
-        height: 3rem;
-        border-radius: 50%;
-        background: #0008;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        line-height: 0;
-        color: var(--bg);
-    }
-
-    .content {
-        border: 1px solid #0002;
-        width: 80vw;
-        height: auto;
-        min-height: 80vh;
-        display: flex;
-        background: var(--white);
-    }
-
-`;
-
-export const Banner = styled.div`
-    @media (max-width: 850px) {
-        display: none;
-    }
-
-    flex: 7;
-    border-right: 1px solid #0002;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    box-sizing: border-box;
-    padding: 1rem 0;
-
-    .container {
-        width: 80%;
-    }
-
-    .logo {
-        position: relative;
-        margin: 0 auto;
-        width: 10rem;
-        aspect-ratio: 9/12;
-    }
-
-    .texts {
-        text-align: center;
-        font-size: 1.8rem;
-        margin-top: 3rem;
-
-        p {
-            color: var(--black);
-        }
-        p + p {
-            font-weight: 300;
-            color: #000a;
-            margin-top: 2rem;
-        }
-    }
-
-    .cards {
-        margin-top: 3rem;
-        display: grid;
-        grid-gap: 2rem;
-        grid-template-columns: repeat(2, 1fr);
-        justify-items: center;
-
-        .card {
-            .image-container {
-                position: relative;
-                width: 5rem;
-                height: 5rem;
-                margin: auto;
-            }
-            p {
-                margin-top: 1rem;
-                text-align: center;
-            }
-        }
-    }
-`;
-
-export const Form = styled.main`
-    flex: 5;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    > div {
-        width: 80%;
-
-        
-    }
-`;
-
 export const GoogleButton = styled.button`
     background: #434FAA;
     color: var(--white);
@@ -147,31 +39,16 @@ export const Separator = styled.div`
     }
 `;
 
-export const InputContainer = styled.div<{isEmpty: boolean}>`
-    position: relative;
-    margin-bottom: 2rem;
-
-    input {
-        border: 2px solid #0008;
-        border-radius: .5rem;
-        background: none;
-        width: 100%;
-        padding: 1rem;
-    }
-
-    
-    > label {
-        transition: all .2s ease;
-        position: absolute;
-        background: var(--white);
-        padding: 0 .5rem;
-        ${props => props.isEmpty ? 'top: 50%' : 'top: 0'};
-        /* top: 50%; */
-        left: 1rem;
-        transform: translateY(-50%);
-    }
-    
-    input:focus ~ label {
-        top: 0;
-    }
+export const SignupButton = styled.button`
+    width: 100%;
+    height: 4rem;
+    background: var(--blue2);
+    border: none;
+    border-radius: .5rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-transform: uppercase;
+    color: var(--white);
+    cursor: pointer;
 `;
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,13 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import * as C from '../app/styles/signup';
 import Image from 'next/image';
-import Logo from '../public/images/Logo2.svg';
-import Icon1 from '../public/images/signup-icon1.svg';
-import Icon2 from '../public/images/signup-icon2.svg';
-import Icon3 from '../public/images/signup-icon3.svg';
-import Icon4 from '../public/images/signup-icon4.svg';
-import Google from '../public/images/google-logo.svg';
-import { useRouter } from 'next/router';
+
+import GoogleLogo from '../public/images/google-logo.svg';
+import { Input } from '../app/components/Input';
+import { AuthLayout } from '../app/patterns/AuthLayout';
 
 type FormType = {
     name: string;
@@ -16,83 +13,112 @@ type FormType = {
     confirmPassword: string;
 }
 
-export default function Signup() {
-    const router = useRouter();
+const initialState = {
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+}
 
-    const [form, setForm] = useState({} as FormType);
+type ReducerState = {
+    name: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+}
+
+enum ActionTypes {
+    UPDATE_NAME = 'update_name',
+    UPDATE_EMAIL = 'update_email',
+    UPDATE_PASSWORD = 'update_password',
+    UPDATE_CONFIRM_PASSWORD = 'update_confirm_password'
+}
+
+type Action = {
+    type: ActionTypes;
+    payload: string;
+}
+
+function reducer(state: ReducerState, action: Action): FormType {
+    switch (action.type) {
+        case ActionTypes.UPDATE_NAME:
+            return {
+                ...state,
+                name: action.payload
+            };
+        case ActionTypes.UPDATE_EMAIL:
+            return {
+                ...state,
+                email: action.payload
+            };
+        case ActionTypes.UPDATE_PASSWORD:
+            return {
+                ...state,
+                password: action.payload
+            };
+        case ActionTypes.UPDATE_CONFIRM_PASSWORD:
+            return {
+                ...state,
+                confirmPassword: action.payload
+            };
+        default:
+            return state;
+    }
+}
+
+export default function Signup() {
+
+    const [state, dispatch] = useReducer(reducer, initialState);
+    const [isPasswordEqual, setIsPasswordEqual] = useState(false);
+
+    useEffect(() => {
+        setIsPasswordEqual(state.password === state.confirmPassword);
+    }, [state])
 
     return (
-        <C.Container>
-            <div className="close-btn" onClick={e => router.back()}>x</div>
-            <div className="content">
-                <C.Banner>
-                    <div className="container">
-                        <div className="logo">
-                            <Image src={Logo} alt="logo" layout="fill" />
-                        </div>
-                        <div className="texts">
-                            <p>Why should I Join Fasthome?</p>
-                            <p>To have the experience of “property”, “information” and “trust” in the real estate world all together…</p>
-                        </div>
-                        <div className="cards">
-                            <div className="card">
-                                <div className="image-container">
-                                    <Image src={Icon1} alt="location icon" layout="fill" />
-                                </div>
-                                <p>Learn about location.</p>
-                            </div>
-                            <div className="card">
-                                <div className="image-container">
-                                    <Image src={Icon2} alt="location icon" layout="fill" />
-                                </div>
-                                <p>Find out the value of your house free of charge.</p>
-                            </div>
-                            <div className="card">
-                                <div className="image-container">
-                                    <Image src={Icon3} alt="location icon" layout="fill" />
-                                </div>
-                                <p>Discover houses that will improve your life quality.</p>
-                            </div>
-                            <div className="card">
-                                <div className="image-container">
-                                    <Image src={Icon4} alt="location icon" layout="fill" />
-                                </div>
-                                <p>Be aware of new projects.</p>
-                            </div>
-                        </div>
-                    </div>
-                </C.Banner>
-                <C.Form>
-                    <div>
-                        <C.GoogleButton>
-                            <div className="google-logo-container"><Image src={Google} alt="google logo" layout="fill" /></div>
-                            <p>Connect with Google</p>
-                        </C.GoogleButton>
-                        <C.Separator>
-                            <div />
-                            <p>or</p>
-                            <div />
-                        </C.Separator>
-                        <C.InputContainer isEmpty={!form.name}>
-                            <input type="text" value={form.name} onChange={e => setForm(prev => {return {...prev, name: e.target.value}})}/>
-                            <label>Name and Surname</label>
-                        </C.InputContainer>
-                        <C.InputContainer isEmpty={!form.email}>
-                            <input type="email" />
-                            <label>Email</label>
-                        </C.InputContainer>
-                        <C.InputContainer isEmpty={!form.password}>
-                            <input type="password" />
-                            <label>Password</label>
-                        </C.InputContainer>
-                        <C.InputContainer isEmpty={!form.confirmPassword}>
-                            <input type="password" />
-                            <label>Confirm Password</label>
-                        </C.InputContainer>
-                        <C.SignupButton>Sign Up</C.SignupButton>
-                    </div>
-                </C.Form>
-            </div>
-        </C.Container>
+        <AuthLayout>
+            <C.GoogleButton>
+                <div className="google-logo-container"><Image src={GoogleLogo} alt="google logo" layout="fill" /></div>
+                <p>Connect with Google</p>
+            </C.GoogleButton>
+            <C.Separator>
+                <div />
+                <p>or</p>
+                <div />
+            </C.Separator>
+            <Input
+                label="Name"
+                value={state.name}
+                dispatch={dispatch}
+                type={ActionTypes.UPDATE_NAME}
+                input={{ type: "text" }}
+            />
+            <Input
+                label="Email"
+                value={state.email}
+                dispatch={dispatch}
+                type={ActionTypes.UPDATE_EMAIL}
+                input={{ type: "email" }}
+            />
+            <Input
+                label="Password"
+                value={state.password}
+                dispatch={dispatch}
+                type={ActionTypes.UPDATE_PASSWORD}
+                input={{ type: "password" }}
+            />
+            <Input
+                label="Confirm Password"
+                value={state.confirmPassword}
+                dispatch={dispatch}
+                type={ActionTypes.UPDATE_CONFIRM_PASSWORD}
+                input={{ type: "password" }}
+            />
+
+            {
+                !isPasswordEqual && <p>passwords do not match</p>
+            }
+            <C.SignupButton>Sign Up</C.SignupButton>
+        </AuthLayout>
     )
 }
