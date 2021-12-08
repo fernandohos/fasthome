69d92ab717eb38d1c4084abca18b0d718db474e6
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2021-12-08 19:19:45 -0300

    profile information page

---
+++ app/styles/profileInformation.ts
@@ -0,0 +1,63 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+
+`;
+
+export const ProfileInfo = styled.div`
+    background: var(--white);
+    width: 88%;
+    margin: auto;
+    margin-top: 2rem;
+    padding: 2rem;
+
+    h1 {
+        font-weight: 500;
+        font-size: 1.8rem;
+    }
+
+    .button {
+        margin: 2rem auto 0;
+        width: min-content;
+        cursor: pointer;
+
+        > button {
+            padding: 1rem 4rem;
+        }
+    }
+`;
+
+export const InputContainer = styled.div`
+    margin-top: 2rem;
+    display: grid;
+    grid-template-columns: 30rem 30rem;
+    grid-column-gap: 2rem;
+`;
+
+export const Textarea = styled.div<{isEmpty: boolean}>`
+    position: relative;
+
+    textarea {
+        border: 2px solid #0008;
+        border-radius: .5rem;
+        background: none;
+        width: 100%;
+        height: 10rem;
+        padding: 1rem;
+        resize: vertical;
+    }
+    textarea:focus ~ label {
+        top: 0;
+    }
+    
+    > label {
+        transition: all .2s ease;
+        position: absolute;
+        background: var(--white);
+        padding: 0 .5rem;
+        ${props => props.isEmpty ? 'top: 50%' : 'top: 0'};
+        left: 1rem;
+        transform: translateY(-50%);
+        user-select: none;
+    }
+`;
---
+++ pages/profile-information.tsx
@@ -0,0 +1,162 @@
+import React, { useReducer } from 'react';
+import * as C from '../app/styles/profileInformation';
+import { Header } from '../app/patterns/Header';
+import { Input } from '../app/components/Input';
+import { Button } from '../app/components/Button';
+
+enum ActionType {
+    UPDATE_NAME = 'update_name',
+    UPDATE_EMAIL = 'update_email',
+    UPDATE_PROVINCE = 'update_province',
+    UPDATE_DISTRICT = 'update_ditrict',
+    UPDATE_MOBILE_NUMBER = 'update_mobile_number',
+    UPDATE_MOBILE_NUMBER2 = 'update_mobile_number2',
+    UPDATE_TELEPHONE = 'update_telephone',
+    UPDATE_ADDRESS = 'update_address'
+}
+
+type StateType = {
+    name: string;
+    email: string;
+    province: string;
+    district: string;
+    mobileNumber: string;
+    mobileNumber2: string;
+    telephone: string;
+    address: string;
+}
+
+type Action = {
+    type: ActionType;
+    payload: string;
+}
+
+function reducer(state: StateType, action: Action) {
+    switch (action.type) {
+        case ActionType.UPDATE_NAME:
+            return {
+                ...state,
+                name: action.payload
+            };
+        case ActionType.UPDATE_EMAIL:
+            return {
+                ...state,
+                email: action.payload
+            };
+        case ActionType.UPDATE_PROVINCE:
+            return {
+                ...state,
+                province: action.payload
+            };
+        case ActionType.UPDATE_DISTRICT:
+            return {
+                ...state,
+                district: action.payload
+            };
+        case ActionType.UPDATE_MOBILE_NUMBER:
+            return {
+                ...state,
+                mobileNumber: action.payload
+            };
+        case ActionType.UPDATE_MOBILE_NUMBER2:
+            return {
+                ...state,
+                mobileNumber2: action.payload
+            };
+        case ActionType.UPDATE_TELEPHONE:
+            return {
+                ...state,
+                telephone: action.payload
+            };
+        case ActionType.UPDATE_ADDRESS:
+            return {
+                ...state,
+                address: action.payload
+            };
+        default:
+            return state;
+    }
+}
+
+const initialState = {
+    name: '',
+    email: '',
+    province: '',
+    district: '',
+    mobileNumber: '',
+    mobileNumber2: '',
+    telephone: '',
+    address: '',
+}
+
+export default function ProfileInformation() {
+    const [state, dispatch] = useReducer(reducer, initialState)
+    return (
+        <C.Container>
+            <Header />
+            <C.ProfileInfo>
+                <h1>Membership Information</h1>
+                <C.InputContainer>
+                    <Input
+                        dispatch={dispatch}
+                        input={{ type: 'text' }}
+                        value={state.name}
+                        label="Name"
+                        type={ActionType.UPDATE_NAME}
+                    />
+                    <Input
+                        dispatch={dispatch}
+                        input={{ type: 'email' }}
+                        value={state.email}
+                        label="Email"
+                        type={ActionType.UPDATE_EMAIL}
+                    />
+                    <Input
+                        dispatch={dispatch}
+                        input={{ type: 'text' }}
+                        value={state.province}
+                        label="Province"
+                        type={ActionType.UPDATE_PROVINCE}
+                    />
+                    <Input
+                        dispatch={dispatch}
+                        input={{ type: 'text' }}
+                        value={state.district}
+                        label="District"
+                        type={ActionType.UPDATE_DISTRICT}
+                    />
+                    <Input
+                        dispatch={dispatch}
+                        input={{ type: 'text' }}
+                        value={state.mobileNumber}
+                        label="Mobile Number"
+                        type={ActionType.UPDATE_MOBILE_NUMBER}
+                    />
+                    <Input
+                        dispatch={dispatch}
+                        input={{ type: 'text' }}
+                        value={state.mobileNumber2}
+                        label="Mobile Number 2"
+                        type={ActionType.UPDATE_MOBILE_NUMBER2}
+                    />
+                    <Input
+                        dispatch={dispatch}
+                        input={{ type: 'text' }}
+                        value={state.telephone}
+                        label="Telephone"
+                        type={ActionType.UPDATE_TELEPHONE}
+                    />
+                </C.InputContainer>
+                <C.Textarea isEmpty={!state.address}>
+                    <textarea id="address" value={state.address} onChange={
+                        e => dispatch({ type: ActionType.UPDATE_ADDRESS, payload: e.target.value })
+                    } />
+                    <label htmlFor="address">Address</label>
+                </C.Textarea>
+                <div className="button">
+                    <Button>Save</Button>
+                </div>
+            </C.ProfileInfo>
+        </C.Container>
+    )
+}
