f37544b2cc976d4c40128df6c3156347feb36441
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2021-12-09 21:26:07 -0300

    dropdown component

---
+++ app/components/Dropdown/index.tsx
@@ -0,0 +1,45 @@
+import React, { ReactNode, useState } from 'react';
+import * as C from './styles';
+import arrowIcon from '../../../public/images/arrow.svg';
+import { AnimatePresence, motion } from 'framer-motion';
+import Image from 'next/image';
+
+type Props = {
+    label: string;
+    children: ReactNode;
+}
+
+export function Dropdown({ label, children }: Props) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <C.Container isOpen={isOpen} onClick={e => setIsOpen(p => !p)}>
+            <div className="summary">
+                <p>
+                    {label}
+                </p>
+                <div className="arrow-container">
+                    <Image src={arrowIcon} alt="arrow icon" layout="fill" />
+                </div>
+            </div>
+            <AnimatePresence>
+                {
+                    isOpen && (
+                        <motion.div
+                            className="detail"
+                            initial={{ opacity: 0, y: -10 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            exit={{ opacity: 0, y: -10 }}
+                            transition={{
+                                default: { duration: .2 },
+                            }}
+                        >
+                            {children}
+                        </motion.div>
+                    )
+                }
+            </AnimatePresence>
+
+        </C.Container>
+    )
+}
---
+++ app/components/Dropdown/styles.ts
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+export const Container = styled.div<{isOpen: boolean}>`
+    position: relative;
+
+    .summary {
+        border-radius: .5rem;
+        border: 1px solid #0003;
+        padding: .7rem;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+        > p {
+            max-width: 90%;
+            white-space: nowrap;
+            overflow: hidden;
+            
+        }
+
+        .arrow-container {
+            position: relative;
+            width: 1.8rem;
+            height: 1.8rem;
+            transition: transform ease .2s;
+            transform: ${props => props.isOpen ? 'rotateX(180deg)': 'rotateX(0deg)'};
+        }
+    }
+
+    .detail {
+        position: absolute;
+        top: 105%;
+        left: 0;
+        width: 100%;
+        background: var(--white);
+        border: 1px solid #0003;
+        border-radius: .5rem;
+        padding: .7rem;
+    }
+`;
