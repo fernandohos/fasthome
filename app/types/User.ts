ab644ea34f79d8c1a6e70917a09d087e2447af1d
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2021-12-15 15:34:55 -0300

    User type

---
+++ app/types/User.ts
@@ -0,0 +1,12 @@
+export type User = {
+    uid: string;
+    displayName: string | null;
+    email: string | null;
+    photoURL: string | null;
+    province: string | null,
+    district: string | null,
+    mobileNumber: string | null,
+    mobileNumber2: string | null,
+    telephone: string | null,
+    address: string | null,
+}
