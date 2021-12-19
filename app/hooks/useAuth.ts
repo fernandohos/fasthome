decbb00a8a2f7fd7e1480b11d9aa7a3d134bdb01
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2021-12-19 19:58:58 -0300

    updating user on auth stage changed

--- app/hooks/useAuth.ts
+++ app/hooks/useAuth.ts
@@ -1,11 +1,13 @@
+import { useEffect } from 'react';
 import {
     signInWithEmailAndPassword,
     createUserWithEmailAndPassword,
     GoogleAuthProvider,
     signInWithPopup,
-    signOut
+    signOut,
+    onAuthStateChanged
 } from 'firebase/auth';
-import { setDoc, doc } from 'firebase/firestore';
+import { setDoc, doc, getDoc } from 'firebase/firestore';
 import { auth, db } from '../services/firebase';
 import Router from 'next/router';
 import { useState } from 'react';
@@ -13,6 +15,36 @@ import { User } from '../types/User';
 
 export default function useAuth() {
     const [user, setUser] = useState<null | User>(null);
+
+    useEffect(() => {
+        const unsub = onAuthStateChanged(auth, async currentUser => {
+            if (currentUser?.uid) {
+                const ref = doc(db, "users", currentUser.uid);
+
+                const userDoc = await getDoc(ref);
+
+                const data = userDoc.data();
+
+                if (data) {
+                    const { uid, displayName, email, photoURL, province, district, mobileNumber, mobileNumber2, telephone, address } = data;
+
+                    setUser({
+                        uid,
+                        displayName,
+                        email,
+                        photoURL,
+                        province,
+                        district,
+                        mobileNumber,
+                        mobileNumber2,
+                        telephone,
+                        address
+                    });
+                }
+            }
+        })
+        return unsub;
+    }, [])
 
     const signIn = async (email: string, password: string) => {
         try {
