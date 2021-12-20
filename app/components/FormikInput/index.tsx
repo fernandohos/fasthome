339ab048ccd3cd08651686ecf99228ee8facf714
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2021-12-20 17:14:40 -0300

    optional required props

--- app/components/FormikInput/index.tsx
+++ app/components/FormikInput/index.tsx
@@ -9,12 +9,13 @@ type Props<T> = {
     value: T;
     error: string | undefined;
     isTextarea?: boolean;
+    required?: boolean
 }
 
-export function FormikInput<T>({ label, name, type, value, error, isTextarea = false }: Props<T>) {
+export function FormikInput<T>({ label, name, type, value, error, isTextarea = false, required = false }: Props<T>) {
     return (
         <C.Container error={!!error} isEmpty={!value}>
-            <Field id={name} as={isTextarea && 'textarea'} type={type} name={name} required />
+            <Field id={name} as={isTextarea && 'textarea'} type={type} name={name} required={required} />
             <label htmlFor={name}>
                 {label}
             </label>
