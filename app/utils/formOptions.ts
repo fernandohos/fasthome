46264aa32b9f09a33fddd44de65eb3d5db0139d2
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2021-12-29 19:19:53 -0300

    form options

---
+++ app/utils/formOptions.ts
@@ -0,0 +1,41 @@
+export const formOptions = {
+    housing: [
+        ['Apartment', 'apartment'],
+        ['Building', 'building'],
+        ['Summery', 'summery'],
+        ['Mansion', 'mansion'],
+        ['Waterside', 'waterside'],
+        ['Villa', 'villa'],
+    ],
+    sale: [['For Sale', 'forSale'], ['For Rental', 'forRental']],
+    warmingType: [
+        ['No Heating', 'noHeating'],
+        ['Natural Gas', 'naturalGas'],
+        ['Solar Energy', 'solarEnergy'],
+        ['Geothermal', 'geothermal'],
+        ['Air Conditioned', 'airConditioned'],
+        ['With Stove', 'withStove'],
+    ],
+    avaliableForLoan: [
+        ['All Of', 'allOf'],
+        ['Avaliable', 'avaliable'],
+        ['Not Avaliable', 'notAvaliable'],
+        ['Unknown', 'unknown'],
+    ],
+    furnished: [
+        ['Furnished', 'furnished'],
+        ['Not Furnished', 'notFurnished'],
+    ],
+    status: [
+        ['Zero', 'zero'],
+        ['Second Hand', 'secondHand'],
+        ['The Under Construction', 'theUnderConstruction'],
+    ],
+    swap: [['All of', 'allOf'], ['Yes', 'yes'], ['No', 'no']],
+    front: [
+        ['North', 'north'],
+        ['South', 'south'],
+        ['East', 'east'],
+        ['West', 'west'],
+    ],
+}
