d4d65f9d5f831421c25c0683ee57e1f29ccda4ec
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2021-12-29 13:53:48 -0300

    fieldset form category

---
+++ app/patterns/FormCategory/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Fieldset } from '../../components/Fieldset';
+import { FormikSelect } from '../../components/FormikSelect';
+import { formOptions } from '../../utils/formOptions';
+import * as C from '../../styles/form';
+
+export function FormCategory() {
+    return (
+        <Fieldset title="Category">
+            <C.FlexWrapper>
+                <C.InputContainer>
+                    <FormikSelect
+                        label="Housing"
+                        name="housing"
+                        options={formOptions.housing}
+                    />
+                </C.InputContainer>
+                <C.InputContainer width="15rem">
+                    <FormikSelect
+                        label="Sale"
+                        name="sale"
+                        options={formOptions.sale}
+                    />
+                </C.InputContainer>
+            </C.FlexWrapper>
+        </Fieldset>
+    )
+}
