7243c7df580015a54ee082e404190f9f6ad455bf
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2021-12-31 17:24:30 -0300

    formik select error message

--- app/components/FormikSelect/index.tsx
+++ app/components/FormikSelect/index.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from 'react';
-import { Field, useField } from 'formik';
+import { Field, useField, ErrorMessage } from 'formik';
 import * as C from './styles';
 
 type Props = {
@@ -11,22 +11,29 @@ type Props = {
 }
 
 export function FormikSelect({ label, name, required = false, options }: Props) {
-    const [inputProps, meta] = useField(name);
+    const [, meta] = useField(name);
     return (
-        <C.Container error={!!meta.error} isEmpty={!meta.value}>
-            <Field id={name} as='select' name={name} required={required}>
-                <option></option>
-                {
-                    options.map((option, i) => (
-                        <option key={i} value={option[1]}>
-                            {option[0]}
-                        </option>
-                    ))
-                }
-            </Field>
-            <label htmlFor={name}>
-                {label}
-            </label>
-        </C.Container>
+        <>
+            <C.Container error={!!meta.error && meta.touched} isEmpty={!meta.value}>
+                <Field id={name} as='select' name={name} required={required}>
+                    <option></option>
+                    {
+                        options.map((option, i) => (
+                            <option key={i} value={option[1]}>
+                                {option[0]}
+                            </option>
+                        ))
+                    }
+                </Field>
+                <label htmlFor={name}>
+                    {label}
+                </label>
+            </C.Container>
+            <ErrorMessage name={name}>
+                {message => (
+                    <C.ErrorMessage>{message}</C.ErrorMessage>
+                )}
+            </ErrorMessage>
+        </>
     )
 }
