ca94cb72801ec32df2c00766a18ca507f983a381
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2022-01-02 17:12:12 -0300

    implementing formik wrapper

--- pages/advertise/form.tsx
+++ pages/advertise/form.tsx
@@ -1,101 +1,42 @@
-import React from 'react'
+import React, { ReactElement } from 'react'
+import { Field } from 'formik';
 import dynamic from 'next/dynamic';
 import * as C from '../../app/styles/form';
 import { Header } from '../../app/patterns/Header';
-import { FormNavBar } from '../../app/patterns/FormNavBar';
-import { Formik, Form, FormikHelpers, Field } from 'formik';
 import { FormCategory } from '../../app/patterns/FormCategory';
 import FormGeneralInformation from '../../app/patterns/FormGeneralInformation';
 import { PostingPhotos } from '../../app/components/PostingPhotos';
 import FormAdvertiseFeatures from '../../app/patterns/FormAdvertiseFeatures';
-import { schema } from '../../app/utils/formValitationSchema';
+import { FormikWrapper } from '../../app/patterns/FormikWrapper';
+import Link from 'next/link';
 
-interface FileType extends File {
-    preview: string;
-}
-
-type ValuesType = {
-    housing: string,
-    sale: string,
-    title: string,
-    explanation: string,
-    price: number,
-    numberOfRoom: number,
-    grossM2: number,
-    netM2: number,
-    warmingType: string,
-    buildingAge: number,
-    floorLocation: number,
-    avaliableForLoan: string,
-    furnished: string,
-    status: string,
-    dues: number,
-    swap: string,
-    front: string,
-    rentalIncome: number,
-    address: string;
-    files: FileType[];
-    externalFeatures: string[],
-    interiorFeatures: string[]
-}
-
-const initialValues: ValuesType = {
-    housing: '',
-    sale: '',
-    title: '',
-    explanation: '',
-    price: 0,
-    numberOfRoom: 0,
-    grossM2: 0,
-    netM2: 0,
-    warmingType: '',
-    buildingAge: 0,
-    floorLocation: 0,
-    avaliableForLoan: '',
-    furnished: '',
-    status: '',
-    dues: 0,
-    swap: '',
-    front: '',
-    rentalIncome: 0,
-    address: '',
-    files: [],
-    interiorFeatures: [],
-    externalFeatures: []
-}
-
-export const Map = dynamic(
+const DynamicMap = dynamic(
     () => import('../../app/patterns/FormLocationInformation'),
     { ssr: false }
 )
 
-export default function AdvertiseForm() {
-    function onSubmit(values: ValuesType, actions: FormikHelpers<ValuesType>) {
-        console.log("SUBMITED", values);
-        actions.resetForm();
-    }
+function AdvertiseForm() {
     return (
-        <>
-            <Header />
-            <C.Container>
-                <FormNavBar />
-                <Formik
-                    initialValues={initialValues}
-                    onSubmit={onSubmit}
-                    validationSchema={schema}
-                >
-                    {() => (
-                        <Form style={{ textAlign: 'center', marginBottom: '4rem' }}>
-                            <FormCategory />
-                            <FormGeneralInformation />
-                            <Map />
-                            <Field component={PostingPhotos} />
-                            <FormAdvertiseFeatures />
-                            <C.Button type="submit">Next</C.Button>
-                        </Form>
-                    )}
-                </Formik>
-            </C.Container>
-        </>
+        <C.Container>
+            <FormCategory />
+            <FormGeneralInformation />
+            <DynamicMap />
+            <Field component={PostingPhotos} />
+            <FormAdvertiseFeatures />
+            <Link href="/advertise/preview" passHref>
+                <C.Button>Next</C.Button>
+            </Link>
+        </C.Container>
     )
 }
+
+AdvertiseForm.getLayout = function getLayout(page: ReactElement) {
+    return (
+        <FormikWrapper>
+            <Header />
+            {page}
+        </FormikWrapper>
+    )
+}
+
+export default AdvertiseForm;
