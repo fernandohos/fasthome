7ebe90e57b442d5f804395b4b709988061df7f73
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2022-01-08 17:40:25 -0300

    form values type

---
+++ app/types/FormValuesType.ts
@@ -0,0 +1,28 @@
+interface FileType extends File {
+    preview: string;
+}
+
+export type FormValuesType = {
+    housing: string,
+    sale: string,
+    title: string,
+    explanation: string,
+    price: number,
+    numberOfRoom: number,
+    grossM2: number,
+    netM2: number,
+    warmingType: string,
+    buildingAge: number,
+    floorLocation: number,
+    avaliableForLoan: string,
+    furnished: string,
+    status: string,
+    dues: number,
+    swap: string,
+    front: string,
+    rentalIncome: number,
+    address: string;
+    files: FileType[];
+    externalFeatures: string[],
+    interiorFeatures: string[]
+}
