1630aa6a28cd7e6e0fec2658244fb25c821b1620
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2022-01-14 19:22:31 -0300

    firebase storage config

--- app/services/firebase.ts
+++ app/services/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -16,3 +17,5 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 
 export const db = getFirestore(app);
+
+export const storage = getStorage(app);
