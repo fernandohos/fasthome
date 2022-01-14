6da5df78a8246c9da7bc026e0d186f32c16b90f8
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2022-01-14 19:25:31 -0300

    upload ad

--- app/patterns/FormikWrapper/index.tsx
+++ app/patterns/FormikWrapper/index.tsx
@@ -1,8 +1,13 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useState } from 'react';
 import { Formik, Form, FormikHelpers } from 'formik';
 import { schema } from '../../utils/formValitationSchema';
 import { FormValuesType } from '../../types/FormValuesType';
 import { useAuth } from '../../hooks/useAuth';
+import { storage, db } from '../../services/firebase';
+import { collection, addDoc } from 'firebase/firestore';
+import imageCompression from 'browser-image-compression';
+import { ref, getDownloadURL, uploadBytesResumable } from '@firebase/storage';
+import { useRouter } from 'next/router';
 
 type Props = {
     children: ReactNode;
@@ -10,6 +15,9 @@ type Props = {
 
 export function FormikWrapper({ children }: Props) {
     const { user } = useAuth();
+    const [uploading, setUploading] = useState(false);
+    const router = useRouter();
+    let imagesUrls: string[] = [];
     const initialValues: FormValuesType = {
         housing: '',
         sale: '',
@@ -33,15 +41,59 @@ export function FormikWrapper({ children }: Props) {
         files: [],
         interiorFeatures: [],
         externalFeatures: [],
-        name: user?.displayName ??'',
+        name: user?.displayName ?? '',
         email: user?.email ?? '',
         mobileNumber: user?.mobileNumber ?? '',
         mobileNumber2: user?.mobileNumber2 ?? '',
         telephone: user?.telephone ?? '',
     }
-    function onSubmit(values: FormValuesType, actions: FormikHelpers<FormValuesType>) {
-        console.log("SUBMITED", values);
-        actions.resetForm();
+
+
+    async function onSubmit(
+        values: FormValuesType,
+        actions: FormikHelpers<FormValuesType>) {
+        setUploading(true);
+
+        const promise2 = new Promise((resolve, reject) => {
+            values.files.map(file => {
+                const storageRef = ref(storage, `/houses/${file.name}`);
+                // compress file
+                imageCompression(file, {
+                    maxSizeMB: .2
+                }).then(compressedFile => {
+                    const uploadTask = uploadBytesResumable(storageRef, compressedFile);
+                    const unsub = uploadTask.on(
+                        "state_changed",
+                        (snapshot: any) => {
+                            const pgrs = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+                        },
+                        (error) => console.log(error),
+                        () => {
+                            getDownloadURL(uploadTask.snapshot.ref).then(url => {
+                                imagesUrls.push(url);
+                                if (imagesUrls.length === values.files.length) {
+                                    resolve(imagesUrls);
+                                }
+                            });
+                        }
+                    )
+                })
+            })
+        });
+
+        Promise.all([promise2]).then(async () => {
+            const collectionRef = collection(db, "houses");
+            const getAdData = () => {
+                const data = JSON.parse(JSON.stringify(values));
+                delete data.files;
+                data.images = imagesUrls;
+                return data;
+            }
+            const docRes = await addDoc(collectionRef, getAdData());
+            actions.resetForm();
+            router.push("/advertise/confirm");
+            setUploading(false);
+        })
     }
     return (
         <Formik
