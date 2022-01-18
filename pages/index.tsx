42593054628c79b5ab512aabf506b962bbeb924f
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2022-01-17 23:21:28 -0300

    fetching featured data

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,72 +5,85 @@ import { Header } from '../app/patterns/Header';
 import { Banner } from '../app/patterns/Banner';
 import { Featured } from '../app/patterns/Featured';
 import { SearchOnMapBanner } from '../app/patterns/SearchOnMapBanner';
+import { FormValuesType } from '../app/types/FormValuesType';
+import { collection, getDocs, limit, query, where } from 'firebase/firestore';
+import { db } from '../app/services/firebase';
 
+interface HouseType extends FormValuesType {
+  id: string;
+  images: string[];
+}
 
-const Home: NextPage = () => {
-  const featuredSales = {
-    title: 'Featured Sales',
-    linkTo: '/for-sale',
-    data: [
-      {
-        id: '123',
-        image: '/images/house.jpg',
-        title: 'apartment for sale',
-        price: 290000,
-        address: 'São Paulo - SP',
-        createdAt: 1638401015738,
-        bedrooms: 2,
-        floor: 4,
-        squareMeters: 140
-      },
-      {
-        id: '1213',
-        image: '/images/house.jpg',
-        title: 'apartment for sale',
-        price: 330000,
-        address: 'São Paulo - SP',
-        createdAt: 1638401015738,
-        bedrooms: 2,
-        floor: 4,
-        squareMeters: 140
-      },
-      {
-        id: '1234',
-        image: '/images/house.jpg',
-        title: 'apartment for sale',
-        price: 290000,
-        address: 'São Paulo - SP',
-        createdAt: 1638401015738,
-        bedrooms: 2,
-        floor: 4,
-        squareMeters: 140
-      },
-      {
-        id: '532',
-        image: '/images/house.jpg',
-        title: 'casinha',
-        price: 250000,
-        address: 'Rio de Janeiro - RJ',
-        createdAt: 1638401015738,
-        bedrooms: 2,
-        floor: 7,
-        squareMeters: 120,
-      }
-    ]
-  }
-  const featuredRental = { ...featuredSales, title: 'Featured Rental', linkTo: "/for-rental" }
+type Props = {
+  rentalData: HouseType[];
+  saleData: HouseType[];
+}
 
+export default function Home({ rentalData, saleData }: Props) {
   return (
     <C.Container>
       <Header />
       <Banner />
       <main>
-        <Featured {...featuredSales} />
-        <Featured {...featuredRental} />
+        <Featured {...{
+          title: 'Featured Sales',
+          linkTo: '/for-sale',
+          data: saleData.map(saleHouse => ({
+            id: saleHouse.id,
+            image: saleHouse.images[0],
+            title: saleHouse.title,
+            price: saleHouse.price,
+            address: saleHouse.address,
+            createdAt: saleHouse.createdAt,
+            bedrooms: saleHouse.numberOfRoom,
+            floor: saleHouse.floorLocation,
+            squareMeters: saleHouse.grossM2
+          }))
+        }} />
+        <Featured {...{
+          title: 'Featured Rental',
+          linkTo: '/for-rental',
+          data: rentalData.map(rentalHouse => ({
+            id: rentalHouse.id,
+            image: rentalHouse.images[0],
+            title: rentalHouse.title,
+            price: rentalHouse.price,
+            address: rentalHouse.address,
+            createdAt: rentalHouse.createdAt,
+            bedrooms: rentalHouse.numberOfRoom,
+            floor: rentalHouse.floorLocation,
+            squareMeters: rentalHouse.grossM2
+          }))
+        }} />
         <SearchOnMapBanner />
       </main>
     </C.Container>
   )
 }
 
-export default Home
+export async function getServerSideProps() {
+  const ref = collection(db, "houses");
+  const saleQuery = query(ref, where("sale", "==", "forSale"), limit(4));
+  const rentalQuery = query(ref, where("sale", "==", "forRental"), limit(4));
+  const saleSnapshot = await getDocs(saleQuery);
+  const rentalSnapshot = await getDocs(rentalQuery);
+
+  const saleData: HouseType[] = [];
+  const rentalData: HouseType[] = [];
+
+  saleSnapshot.forEach((doc) => {
+    const docData = doc.data() as HouseType;
+    docData.id = doc.id;
+    saleData.push(docData);
+  })
+
+  rentalSnapshot.forEach((doc) => {
+    const docData = doc.data() as HouseType;
+    docData.id = doc.id;
+    rentalData.push(docData);
+  })
+
+  return {
+    props: { saleData, rentalData }
+  }
+}
