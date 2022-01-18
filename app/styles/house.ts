484bc663cdb44822e9a8af0991a3787c60bfef85
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2022-01-18 19:49:17 -0300

    ad page aside

--- app/styles/house.ts
+++ app/styles/house.ts
@@ -95,3 +95,48 @@ export const FeaturesContainer = styled.div`
     display: grid;
     grid-template-columns: repeat(2, 1fr);
 `;
+
+export const OwnerCard = styled.div`
+    background: #fff;
+    padding: 1.5rem;
+    display: flex;
+    border: 1px solid #0003;
+    flex-direction: column;
+    align-items: center;
+
+    .user-image {
+        border: 3px solid var(--yellow);
+        width: 7rem;
+        height: 7rem;
+        position: relative;
+        border-radius: 50%;
+        overflow: hidden;
+    }
+
+    > p {
+        margin-top: 1rem;
+        font-weight: 500;
+        font-size: 1.7rem;
+    }
+`;
+
+export const InfoButton = styled.div<{color?: string, border?: string, background?: string}>`
+    padding: 1rem;
+    display: flex;
+    align-items: center;
+    height: 4rem;
+    gap: 1rem;
+    background: ${props => props.background};
+    border: 2px solid ${props => props.border};
+    color: ${props => props.color};
+    border-radius: .5rem;
+    cursor: pointer;
+    margin-top: 1rem;
+    width: 100%;
+    text-transform: uppercase;
+
+    p {
+        flex: 1;
+        text-align: center;
+    }
+`;
--- pages/house/[id]/index.tsx
+++ pages/house/[id]/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FormValuesType } from '../../../app/types/FormValuesType';
 import mapIcon from '../../../public/images/map-icon.svg';
 import { Header } from '../../../app/patterns/Header';
@@ -9,9 +9,14 @@ import { GetStaticPropsContext } from 'next';
 import Image from 'next/image';
 import { Carousel } from '../../../app/components/Carousel';
 import { Fieldset } from '../../../app/components/Fieldset';
+import defaultUserImage from '../../../public/images/default-user-image.svg';
+import phoneIcon from '../../../public/images/phone-icon.svg';
+import emailIcon from '../../../public/images/email-icon.svg';
 
 interface HouseType extends FormValuesType {
     images: string[];
+    userId: string;
+    userPhotoUrl: string;
 }
 
 type Props = {
@@ -19,6 +24,13 @@ type Props = {
 }
 
 export default function House({ data }: Props) {
+    const [showPhone, setShowPhone] = useState(false);
+
+    function copyText(text: string) {
+        if (navigator.clipboard)
+            navigator.clipboard.writeText(text);
+    }
+
     return (
         <>
             <Header />
@@ -51,28 +63,28 @@ export default function House({ data }: Props) {
                             />
                             <Fieldset title='General Information'>
                                 <C.GenInfoGrid>
-                                        <p>Published Date</p>
-                                        <p>{data.createdAt}</p>
-                                        <p>Floor Location</p>
-                                        <p>{data.floorLocation}</p>
-                                        <p>Advertise Status</p>
-                                        <p>{data.sale}</p>
-                                        <p>Furnished</p>
-                                        <p>{data.furnished}</p>
-                                        <p>Housing Shape</p>
-                                        <p>{data.housing}</p>
-                                        <p>Dues</p>
-                                        <p>{data.dues}</p>
-                                        <p>Room</p>
-                                        <p>{data.numberOfRoom}</p>
-                                        <p>Front</p>
-                                        <p>{data.front}</p>
-                                        <p>Gross / Net M²</p>
-                                        <p>{data.grossM2} / {data.netM2}</p>
-                                        <p>Warming Type</p>
-                                        <p>{data.warmingType}</p>
-                                        <p>Building Age</p>
-                                        <p>{data.buildingAge}</p>
+                                    <p>Published Date</p>
+                                    <p>{data.createdAt}</p>
+                                    <p>Floor Location</p>
+                                    <p>{data.floorLocation}</p>
+                                    <p>Advertise Status</p>
+                                    <p>{data.sale}</p>
+                                    <p>Furnished</p>
+                                    <p>{data.furnished}</p>
+                                    <p>Housing Shape</p>
+                                    <p>{data.housing}</p>
+                                    <p>Dues</p>
+                                    <p>{data.dues}</p>
+                                    <p>Room</p>
+                                    <p>{data.numberOfRoom}</p>
+                                    <p>Front</p>
+                                    <p>{data.front}</p>
+                                    <p>Gross / Net M²</p>
+                                    <p>{data.grossM2} / {data.netM2}</p>
+                                    <p>Warming Type</p>
+                                    <p>{data.warmingType}</p>
+                                    <p>Building Age</p>
+                                    <p>{data.buildingAge}</p>
                                 </C.GenInfoGrid>
                             </Fieldset>
                             <Fieldset title="Explanation">
@@ -100,7 +112,38 @@ export default function House({ data }: Props) {
                             </Fieldset>
                         </C.Content>
                         <C.Owner>
-                            {data.name}
+                            <C.OwnerCard>
+                                <div className="user-image">
+                                    <Image
+                                        layout="fill"
+                                        src={data.userPhotoUrl ?? defaultUserImage}
+                                        alt={data.name}
+                                    />
+                                </div>
+                                <p>{data.name}</p>
+                                <C.InfoButton
+                                    background="var(--green2)"
+                                    color="#fff"
+                                    onClick={e => setShowPhone(p => !p)}
+                                >
+                                    {showPhone ? (
+                                        <p>{data.mobileNumber}</p>
+                                    ) : (
+                                        <>
+                                            <Image src={phoneIcon} width={20} height={20} alt="phone icon" />
+                                            <p>view phone</p>
+                                        </>
+                                    )}
+                                </C.InfoButton>
+                                <C.InfoButton
+                                    border="var(--dark-blue)"
+                                    color="var(--dark-blue)"
+                                    onClick={e => copyText(data.email)}
+                                >
+                                    <Image src={emailIcon} width={20} height={20} alt="phone icon" />
+                                    <p>copy email</p>
+                                </C.InfoButton>
+                            </C.OwnerCard>
                         </C.Owner>
                     </C.Container>
                 ) : (
