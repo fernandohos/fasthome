623fc63f308912c6a69a5f2a01c12cfd12c2fe20
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2022-01-29 19:33:32 -0300

    supabase config

---
+++ app/services/supabase.ts
@@ -0,0 +1,3 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient('https://sakeohyxietmvdzfvzjq.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNjQzMTI5MzAwLCJleHAiOjE5NTg3MDUzMDB9.sWvzvgn3-m_YM4vf9iIhFdP5iXIQqzSEgzOmkBCewIY');
