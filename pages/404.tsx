79b29df23039c7ee62aa56ea213f2e0550ea6cc1
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2022-02-01 22:17:54 -0300

    404 page title

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,10 +1,14 @@
 import logo from '../public/images/Logo.svg';
 import Image from 'next/image';
 import * as C from '../app/styles/404';
+import Head from 'next/head';
 
 export default function Custom404() {
     return (
         <C.Container>
+            <Head>
+                <title>404 | Not found</title>
+            </Head>
             <C.Title>404</C.Title>
             <C.Separator />
             <p>page not found</p>
