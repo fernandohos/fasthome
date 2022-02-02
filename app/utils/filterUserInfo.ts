9277e68cda2f10ff19ac5a057129bd76fc5e9dbd
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2022-02-01 22:03:13 -0300

    geting photo_url with google provider

--- app/utils/filterUserInfo.ts
+++ app/utils/filterUserInfo.ts
@@ -5,7 +5,16 @@ import { User as SupabaseUser } from "@supabase/supabase-js";
 export function filterUserInfo(signedUser: SupabaseUser | null): User | null {
     if (signedUser) {
         const { id, created_at, email = null, user_metadata } = signedUser;
-        const { mobile_number_2, photo_url, province, telephone, mobile_number, display_name, address, district } = user_metadata;
+        let { mobile_number_2, photo_url, province, telephone, mobile_number, display_name, address, district } = user_metadata;
+
+        if(signedUser.app_metadata.provider === 'google') {
+            const identities = signedUser?.identities
+            if(identities) {
+                photo_url = identities[0].identity_data.avatar_url;
+                display_name = identities[0].identity_data.full_name;
+            }
+        }
+
         return {
             id,
             created_at,
