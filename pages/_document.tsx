eeb351fc3bd6d57d634686b611e7fde10a5053cf
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2022-02-03 00:03:50 -0300

    favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -32,6 +32,15 @@ export default class MyDocument extends Document {
         return (
             <Html>
                 <Head>
+                    <link rel="apple-touch-icon" sizes="114x114" href="/favicon/apple-touch-icon.png" />
+                    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+                    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+                    <link rel="manifest" href="/favicon/site.webmanifest" />
+                    <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+                    <link rel="shortcut icon" href="/favicon/favicon.ico" />
+                    <meta name="msapplication-TileColor" content="#00aba9" />
+                    <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
+                    <meta name="theme-color" content="#ffffff" />
                     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap" rel="stylesheet" />
                     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.1/dist/leaflet.css" />
                 </Head>
