1410322d85a8985e5003615533c8a047c800473b
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2023-01-23 21:09:32 -0300

    add search input with autocomplete to banner

--- app/patterns/Banner/index.tsx
+++ app/patterns/Banner/index.tsx
@@ -1,19 +1,11 @@
 import * as C from './styles';
-import searchIcon from '@images/search-icon.svg';
-import Image from 'next/image';
+import { SearchInput } from '@components/SearchInput';
 
 export function Banner() {
     return (
         <C.Banner>
             <h1>Your dream <br /> house is here.</h1>
-            <C.InputContainer action="/search">
-                <input placeholder="search..." type="text" name="q" />
-                <button type="submit" className="icon-container">
-                    <div className="icon">
-                        <Image src={searchIcon} alt="search icon" layout="fill" />
-                    </div>
-                </button>
-            </C.InputContainer>
+            <SearchInput />
         </C.Banner >
     )
 }
